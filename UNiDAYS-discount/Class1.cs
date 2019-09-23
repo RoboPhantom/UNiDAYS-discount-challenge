0608eb7b79e3352af202d04b920c98f68de39702
RoboPhantom <43865421+RoboPhantom@users.noreply.github.com>
2019-09-23 23:59:24 +0100

    Initial class addition

---
+++ UNiDAYS-discount/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UNiDAYS_discount
+{
+    private class UnidaysDiscountChallenge
+    {
+        string basket;
+        private AddToBasket(string Item)
+        {
+
+        }
+
+
+
+        private CalculateTotalPrice()
+        {
+            private double Total;
+            private double DeliveryCharge;
+
+            return Total;
+        }
+    }
+}
