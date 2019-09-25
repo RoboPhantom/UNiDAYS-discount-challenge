cac91a918c15adb6436e3d1c9bcc05591ccfd406
RoboPhantom <43865421+RoboPhantom@users.noreply.github.com>
2019-09-25 13:45:26 +0100

    Added pricing rules to class initialisation. Cleaned up code. Added comments to describe code.

--- Unidays Challenge/Unidays Challenge/Form1.cs
+++ Unidays Challenge/Unidays Challenge/Form1.cs
@@ -18,16 +18,17 @@ namespace Unidays_Challenge
         }
 
         UnidaysDiscountChallenge example;
+        Discounts pricing;
 
         public void Form1_Load(object sender, EventArgs e)
         {
-            //UnidaysDiscountChallenge example = new UnidaysDiscountChallenge();
-            example = new UnidaysDiscountChallenge();
+            pricing = new Discounts();
+            Object[,] PricingRules = pricing.GetPrices();
+            example = new UnidaysDiscountChallenge(PricingRules);
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            //example.AddToBasket(txtAdd.Text);
             txtNotice.Text = example.AddToBasket(txtAdd.Text);
             txtBasket.Text += txtAdd.Text;
             txtAdd.Text = "";
@@ -52,43 +53,35 @@ namespace Unidays_Challenge
 
     class Discounts
     {
-        // [item, default price, discount quantity, discount price]
-        Object[,] Pricing = { { "A", 8, 1, 8 }, { "B", 12, 2, 20 }, { "C", 4, 3, 10 }, { "D", 7, 2, 7 }, { "E", 5, 3, 10 } };
-        public Object[,] PricingRules()
+        // [item, default price, quantity needed for discount, discount price]
+        Object[,] Prices = { { "A", 8, 1, 8 }, { "B", 12, 2, 20 }, { "C", 4, 3, 10 }, { "D", 7, 2, 7 }, { "E", 5, 3, 10 } };
+        public Object[,] GetPrices()
         {
-            return Pricing;
+            return Prices;
         }
     }
 
     class UnidaysDiscountChallenge
     {
+        //Array for pricing rules
         Object[,] Rules;
-        //Object[,] basket;
+
+        //Lists for items and their quantities
         List<string> basket;
         List<int> amount;
+
+        //total price and delivery price
         public double total;
         public double delivery;
 
-        public UnidaysDiscountChallenge()
+        public UnidaysDiscountChallenge(Object[,] prices)
         {
-            Discounts prices = new Discounts();
-            Rules = prices.PricingRules();
+            Rules = prices;
 
             basket = new List<string>();
             amount = new List<int>();
 
-            total = 0;
-            delivery = 7;
-            //basket = new Object[Rules.GetLength(0), 2];
-            // array of items (string) and quantities (int)
-            //basket[0, 0] = "A";
-            //basket[0, 1] = 0;
-        }
-
-        public void Reset()
-        {
-            basket.Clear();
-            amount.Clear();
+            //set base totals
             total = 0;
             delivery = 7;
         }
@@ -96,12 +89,16 @@ namespace Unidays_Challenge
         public string AddToBasket(string Item)
         {
             Boolean exists = false;
+            //Check if basket is empty
             if(basket.Count > 0)
             {
+                //Loops basket items
                 for (int x = 0; x <= basket.Count - 1; x++)
                 {
+                    //Checks if basket item is same as new item
                     if (basket[x] == Item)
                     {
+                        //Increases item amount
                         amount[x] += 1;
                         exists = true;
                         return basket[x] + " added. Amount " + amount[x] + " Basket has " + basket.Count + " different items.";
@@ -109,8 +106,10 @@ namespace Unidays_Challenge
                 }
             }
 
+            // Checks if no matching item
             if (exists == false)
             {
+                //Adds item and amount
                 basket.Add(Item);
                 amount.Add(1);
                 return basket[basket.Count-1] + " added. Amount " + amount[amount.Count-1] + " Basket has " + basket.Count + " different items.";
@@ -121,8 +120,8 @@ namespace Unidays_Challenge
 
         public List<double> CalculateTotalPrice()
         {
+            // Totals list
             List<double> Out = new List<double>();
-            //Object[,] List = basket;
 
             // Loop basket items
             for (int a = 0; a <= basket.Count - 1; a++)
@@ -133,15 +132,19 @@ namespace Unidays_Challenge
                     // Check items match - basket and rule
                     if (basket[a] == (string)Rules[b, 0])
                     {
+                        // Loops item amounts til empty
                         while (amount[a] > 0)
                         {
+                            // Checks if item amount qualifies for discount amount
                             if (amount[a] >= (int)Rules[b, 2])
                             {
+                                //Adds discount price and removes discounted items
                                 total += (int)Rules[b, 3];
                                 amount[a] -= (int)Rules[b, 2];
                             }
                             else
                             {
+                                //Adds default price and removes item
                                 total += (int)Rules[b, 1];
                                 amount[a] -= 1;
                             }
@@ -149,7 +152,7 @@ namespace Unidays_Challenge
                     }
                 }
             }
-
+            //Checks if qualified for free delivery
             if (total >= 50)
             {
                 delivery = 0;
@@ -159,6 +162,15 @@ namespace Unidays_Challenge
             Out.Add(delivery);
             return Out;
 
+        }
+
+        //Clears basket and resets totals
+        public void Reset()
+        {
+            basket.Clear();
+            amount.Clear();
+            total = 0;
+            delivery = 7;
         }
     }
 
